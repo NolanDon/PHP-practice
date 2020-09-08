477e78ed241abc6fd50fc43efea9d319bf28e172
Nolan <nolan.boxill@neofinancial.com>
2020-09-07 23:29:52 -0600

    public functions in classes

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
 <?php 
     // $rover = new Person();
     // echo 'my owner is: '.$rover->owner();
+    // $cookie = new Person();
+    // echo $cookie->owner();
 
-    $cookie = new Person();
-    echo $cookie->owner();;
+    $me = new Person();
+    $me->setName("Devon");
+    echo $me->name;
+
+    $me2 = new Person();
+    $me2->setName("Timmy");
+    echo $me2->name;
+
 ?>
 </body>
 </html>
--- person.php
+++ person.php
@@ -1,9 +1,15 @@
 <?php
     class Person {
+        public $name = "Nolan";
+        public $eyeColor = "Green";
         protected $first = "Daniel";
         private $last = "Nielsen";
         private $age = "29";
         
+        public function setName($name) {
+            $this->name = $name;
+        }
+
         public function owner() {
             $a = $this->first;
             return $a;
